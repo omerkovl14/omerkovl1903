a87ae5861fac5c20169951771fa59d0fe13b97f1
omerkovl14 <120605402+omerkovl14@users.noreply.github.com>
2023-01-11 00:46:33 +0300

    Update Login.java

--- src/UiDesing/Login.java
+++ src/UiDesing/Login.java
@@ -125,7 +125,7 @@ public class Login extends javax.swing.JFrame {
             } catch (Exception e) {
             }
         }
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }
 
     
     public static void main(String args[]) {
